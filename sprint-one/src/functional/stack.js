390ef64f27a69a60c4b7c73855e894b428ca8ae7
Chris Roback <RobackChristopher@gmail.com>
2020-01-03 17:09:37 -0500

    completed part 2

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,23 +2,22 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  // var storage = {};
   let length = 0;
-  
+  let storage = {};
+
  // var length = Object.size(storage);
   // Implement the methods below
-  //ALL OF THESE INSTANCES ARE BEING ASSIGNED TO THE VALUE OF A NEW PROPERTY IN SOME INSTANCE 
+  //ALL OF THESE INSTANCES ARE BEING ASSIGNED TO THE VALUE OF A NEW PROPERTY IN SOME INSTANCE
   someInstance.push = function(value) {//push key value into storage,
-    
-    storage[length] = value;//temporary var for storing a key value pair, 
+    storage[length] = value;//temporary var for storing a key value pair,
     length ++;             //then increment the key and return temp
     return length;
-    
   };
 
   someInstance.pop = function() {
     if (length > 0){             //if length var is greater than 0
-    let temp = storage[length-1];  //assign a temp to the most recent var 
+    let temp = storage[length-1];  //assign a temp to the most recent var
     //delete storage[length-1];     //MUST INCLUDE -1 FOR THEE LAST ELEMENT
     length--;                    //decrement length bc you popped the value
     return temp;
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,27 +1,38 @@
 var Queue = function() {
   this.length = 0;
-  this.oldProp = 0; 
-  this.newProp = 0; 
-  this.storage = {}; 
+  this.oldProp = 0;
+  this.newProp = 0;
+  this.storage = {};
 
 };
 
 Queue.prototype.enqueue = function(value){
-  this.length ++;              //increase length when adding prop
-  this.storage[this.newProp] = value;//store a new property in storage
-  this.newProp++;  
+  // this.length ++;              //increase length when adding prop
+  // this.storage[this.newProp] = value;//store a new property in storage
+  // this.newProp++;
+  this.storage[this.newProp] = value;
+  this.length++;
+  this.newProp++;
 };
 
 Queue.prototype.dequeue = function(){
-  if (this.length !== 0){
-    this.length--;
+  // if (this.length !== 0){
+  //   this.length--;
+  // }
+  // var temp = this.storage[this.oldProp];//set temp var equal to oldest prop
+  // //delete this.storage[this.oldProp];
+  // this.oldProp++;//increase oldes prop key
+  // return temp; //return that stored value
+
+  if (this.length > 0){
+  let temp = this.storage[this.oldProp];//needs to be in brackets beause oldProp is not a part of storage
+  delete this.storage[this.oldProp];
+  this.length--;
+  this.oldProp++;
+  return temp;
   }
-  var temp = this.storage[this.oldProp];//set temp var equal to oldest prop
-  //delete this.storage[this.oldProp];
-  this.oldProp++;//increase oldes prop key
-  return temp; //return that stored value
 };
 
 Queue.prototype.size = function(){
-      return this.length || 0;
+      return this.length;
 };
